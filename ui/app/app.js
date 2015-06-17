1d4d40788b0e5431e07c337df9b78951768bd314
Alex Creasy <acreasy@redhat.com>
2015-06-17 18:48:28 +0100

    [NCL-918] centralised UI error messages

--- ui/app/app.js
+++ ui/app/app.js
@@ -99,6 +99,8 @@
       }
     });
 
+    $httpProvider.interceptors.push('errorInterceptor');
+
     if (PROPERTIES.AUTH_ENABLED) {
       keycloakProvider.setKeycloak(keycloak);
       $httpProvider.interceptors.push('authInterceptor');
