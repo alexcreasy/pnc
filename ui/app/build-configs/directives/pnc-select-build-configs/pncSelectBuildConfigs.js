a9adb16e05d3ce7785b1430e36e57890dec06472
Alex Creasy <acreasy@redhat.com>
2017-08-14 14:03:08 +0100

    Y U NO WERK?

--- ui/app/build-configs/directives/pnc-select-build-configs/pncSelectBuildConfigs.js
+++ ui/app/build-configs/directives/pnc-select-build-configs/pncSelectBuildConfigs.js
@@ -4,17 +4,36 @@
   angular.module('pnc.build-configs').component('pncSelectBuildConfigs', {
     templateUrl: 'build-configs/directives/pnc-select-build-configs/pnc-select-build-configs.html',
     bindings: {
+      /**
+       *
+       *
+       */
       onAdd: '&',
-      onRemove: '&'
+      /**
+       *
+       */
+      onRemove: '&',
+      /**
+       *
+       */
+      collectById: '@',
+      /**
+       * {Array} of BuildConfig objects. This parameter is required if and only if
+       * collect-by-id=true is used. It is an array of the BuildConfigs to initially
+       * populate
+       */
+      initialValues: '<'
     },
     require: {
       ngModel: '?ngModel'
     },
-    controller: ['$log', '$scope', Controller]
+    controller: ['$log', '$scope', 'utils', Controller]
   });
 
-  function Controller($log, $scope) {
+  function Controller($log, $scope, utils) {
     var $ctrl = this,
+
+        collectById = utils.parseBoolean($ctrl.collectById),
 
         listConfig = {
          selectItems: false,
@@ -46,38 +65,75 @@
     // --------------------
 
 
-    $ctrl.$onInit = function () {
-      // if (!angular.isArray($ctrl.buildConfigs)) {
-      //   $ctrl.buildConfigs = [];
-      // }
+    $ctrl.$postLink = function () {
+      if ($ctrl.ngModel) {
 
-      $scope.$watch(function () {
-        return hash($ctrl.buildConfigs);
-      }, function () {
-        $ctrl.ngModel.$setViewValue($ctrl.buildConfigs);
-      });
+        $scope.$watch(function () {
+          return hash($ctrl.buildConfigs);
+        }, function () {
+          $ctrl.ngModel.$setViewValue($ctrl.buildConfigs);
+        });
 
-      // $ctrl.ngModel.$parsers.push(function (viewValue) {
-      //   if (angular.isObject(viewValue) && angular.isDefined($ctrl.modelProperty)) {
-      //     return viewValue[$ctrl.modelProperty];
-      //   }
-      //
-      //   return viewValue;
-      // });
 
-      // Respond to programmatic changes to the ng-model value, to propagate
-      // changes back to the combobox's displayed value.
-      $ctrl.ngModel.$render = function () {
-        // if (!$ctrl.ngModel.$isEmpty($ctrl.ngModel.$viewValue) && angular.isDefined($ctrl.modelProperty)) {
-        //   $log.warn('pnc-build-config-combobox: programatic changing of the ng-model value is not fully supported when model-value parameter is used. The display value of the combobox will not be correctly mapped');
-        // }
+        $ctrl.ngModel.$parsers.push(function (viewValue) {
+          if (collectById) {
+            return viewValue.map(function (bc) { return bc.id; });
+          }
 
-        if (!$ctrl.ngModel.$isEmpty($ctrl.ngModel.$viewValue)) {
-          $ctrl.buildConfigs = $ctrl.ngModel.$viewValue;
-        } else {
-          $ctrl.buildConfigs = [];
+          return viewValue;
+        });
+
+        $ctrl.ngModel.$parsers.push(function (viewValue) {
+          return viewValue;
+        });
+
+        /*
+         * When the model value is changed, if collect-by-id=true is set we
+         * need to map the array of ids back to BC objects to display those to
+         * the user.
+         */
+        $ctrl.ngModel.$formatters.push(function (modelValue) {
+          if (angular.isArray(modelValue)) {
+
+            if (collectById) {
+
+              return modelValue.map(function (id) {
+                return $ctrl.initialValues.find(function (bc) {
+                  return bc.id === id;
+                });
+              });
+
+            }
+
+            return modelValue;
+          }
+
+          return [];
+        });
+
+        $ctrl.ngModel.$render = function () {
+
+          if (!$ctrl.ngModel.$isEmpty($ctrl.ngModel.$viewValue)) {
+            $ctrl.buildConfigs = $ctrl.ngModel.$viewValue;
+
+          // } else if (collectById) {
+
+
+            // $ctrl.buildConfigs = $ctrl.ngModel.$viewValue.map(function (id) {
+            //   return $ctrl.initialValues.find(function (bc) {
+            //     return bc.id === id;
+            //   });
+            // });
+
+          } else {
+            $ctrl.buildConfigs = [];
+          }
+        };
+
+        if (collectById && $ctrl.initialValues && $ctrl.initialValues.length > 0) {
+          $ctrl.buildConfigs = $ctrl.initialValues.map(function (bc) { return bc.id; });
         }
-      };
+      }
     };
 
     function add(buildConfig) {
@@ -96,6 +152,7 @@
       }
       $ctrl.onRemove({ buildConfig: buildConfig });
     }
+
 
     function hash(buildConfigs) {
       var result = 17;
