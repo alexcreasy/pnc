41c52ea42e0aec00f97f1610e4969be6d99855d1
Alex Creasy <acreasy@redhat.com>
2017-08-14 11:48:51 +0100

    Fixup ng-model integration for pnc-select-build-configs

--- ui/app/build-configs/directives/pnc-select-build-configs/pncSelectBuildConfigs.js
+++ ui/app/build-configs/directives/pnc-select-build-configs/pncSelectBuildConfigs.js
@@ -3,15 +3,17 @@
 
   angular.module('pnc.build-configs').component('pncSelectBuildConfigs', {
     templateUrl: 'build-configs/directives/pnc-select-build-configs/pnc-select-build-configs.html',
-    controller: Controller,
     bindings: {
-      buildConfigs: '=ngModel',
       onAdd: '&',
       onRemove: '&'
-    }
+    },
+    require: {
+      ngModel: '?ngModel'
+    },
+    controller: ['$log', '$scope', Controller]
   });
 
-  function Controller() {
+  function Controller($log, $scope) {
     var $ctrl = this,
 
         listConfig = {
@@ -35,6 +37,7 @@
 
     // -- Controller API --
 
+    $ctrl.buildConfigs = [];
     $ctrl.listConfig = listConfig;
     $ctrl.listActionButtons = listActionButtons;
     $ctrl.add = add;
@@ -44,9 +47,37 @@
 
 
     $ctrl.$onInit = function () {
-      if (!angular.isArray($ctrl.buildConfigs)) {
-        $ctrl.buildConfigs = [];
-      }
+      // if (!angular.isArray($ctrl.buildConfigs)) {
+      //   $ctrl.buildConfigs = [];
+      // }
+
+      $scope.$watch(function () {
+        return hash($ctrl.buildConfigs);
+      }, function () {
+        $ctrl.ngModel.$setViewValue($ctrl.buildConfigs);
+      });
+
+      // $ctrl.ngModel.$parsers.push(function (viewValue) {
+      //   if (angular.isObject(viewValue) && angular.isDefined($ctrl.modelProperty)) {
+      //     return viewValue[$ctrl.modelProperty];
+      //   }
+      //
+      //   return viewValue;
+      // });
+
+      // Respond to programmatic changes to the ng-model value, to propagate
+      // changes back to the combobox's displayed value.
+      $ctrl.ngModel.$render = function () {
+        // if (!$ctrl.ngModel.$isEmpty($ctrl.ngModel.$viewValue) && angular.isDefined($ctrl.modelProperty)) {
+        //   $log.warn('pnc-build-config-combobox: programatic changing of the ng-model value is not fully supported when model-value parameter is used. The display value of the combobox will not be correctly mapped');
+        // }
+
+        if (!$ctrl.ngModel.$isEmpty($ctrl.ngModel.$viewValue)) {
+          $ctrl.buildConfigs = $ctrl.ngModel.$viewValue;
+        } else {
+          $ctrl.buildConfigs = [];
+        }
+      };
     };
 
     function add(buildConfig) {
@@ -66,6 +97,25 @@
       $ctrl.onRemove({ buildConfig: buildConfig });
     }
 
+    function hash(buildConfigs) {
+      var result = 17;
+
+      buildConfigs.forEach(function (buildConfig) {
+        var c;
+
+        if (typeof buildConfig === 'number') {
+          c = buildConfig;
+        } else if (angular.isObject(buildConfig) && angular.isDefined(buildConfig.id)) {
+          c = buildConfig.id;
+        } else {
+          throw new TypeError('Unable to hash illegal type: expected a BuildConfig (object) or a BuildConfig id (number), but recieved: ' + buildConfig);
+        }
+
+        result = 31 * result + c;
+      });
+
+      return result;
+    }
   }
 
 })();
